63bcff080ebf89de4c004b31dd8ff7e5158f7faa
Sean-Xiao-Liu <glxiaoliu@gmail.com>
2025-06-07 16:12:44 -0400

    update css files with host

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -5,8 +5,12 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './server-status.component.html',
-  styleUrl: './server-status.component.css'
+  styleUrl: './server-status.component.css',
+  host: {
+    class: 'status',
+    id: 'status'
+  }
 })
 export class ServerStatusComponent {
-  currentStatus = 'online';
+  currentStatus = 'offline';
 }
