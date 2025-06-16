1beadc15fd8efa22bae0ec907a487c63bcf86922
Sean-Xiao-Liu <glxiaoliu@gmail.com>
2025-06-16 16:52:11 -0400

    add onClick() method for server status

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -12,5 +12,32 @@ import { Component } from '@angular/core';
   }
 })
 export class ServerStatusComponent {
-  currentStatus = 'offline';
+  // @Input is used to pass data from parent component to child component, 
+  // in this case, the parent component is the dashboard component 
+  // and the child component is the server-status component
+  @Input() currentStatus = 'offline';
+  @Input() statusClassValue = 'status-offline';
+
+  onStatusClick() {
+    const options = ['online', 'offline', 'unknown'];
+    this.currentStatus = getRandomString(options, this.currentStatus);
+    // this.statusClassValue = getStatusClassValue(this.currentStatus);
+  }
+}
+
+function getRandomString(arr: string[], current: string): string {
+  const filtered = arr.filter(item => item !== current); // get all other status expcept the current one
+  if (filtered.length === 0) throw new Error("No other values to choose from.");
+  const randomIndex = Math.floor(Math.random() * filtered.length);
+  return filtered[randomIndex];
+}
+
+function getStatusClassValue(currentStatus: string): string {
+  if (currentStatus === 'online') {
+    return 'status-online';
+  } else if (currentStatus === 'offline') {
+    return 'status-offline';
+  } else {
+    return 'status-unknown';
+  }
 }
