f6f3bc70b50f4bf130e444a10e8a27c660578292
Sean-Xiao-Liu <glxiaoliu@gmail.com>
2025-06-19 14:18:01 -0400

    introduce @ViewChild decorator to new-ticket component

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from '../../../shared/control/control.component';
 import { FormsModule } from '@angular/forms';
@@ -17,10 +17,19 @@ export class NewTicketComponent {
   //   console.log("entered request: ", this.enteredRequest);
   // }
 
+  // ViewChild is used to get a reference to the form element
+  // so the component can access the form element without passing it as a parameter
+  // the question mark is used to make the form optional
+  @ViewChild('form') form ?: ElementRef<HTMLFormElement>;
+
   onSubmit(enteredTitle: HTMLInputElement, enteredRequest: HTMLTextAreaElement) {
+
     console.log('submit button clicked');
     console.log("entered title: ", enteredTitle.value);
     console.log("entered request: ", enteredRequest.value);
+    // the question mark means it would only reset the form if it is not null
+    this.form?.nativeElement.reset(); // reset the form to clear the input fields
+    console.log("form resetted");
   }
 
   enteredTitle = '';
